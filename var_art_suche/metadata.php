00d79cb8e695654c07ce030a12b5d704a3141c32
Gajel <40059967+Gajel@users.noreply.github.com>
2018-06-08 15:52:26 +0200

    update 10

--- var_art_suche/metadata.php
+++ var_art_suche/metadata.php
@@ -11,9 +11,8 @@ $aModule = array(
     'title'       => 'Variantensuche',
     'description' => 'Variantensuche per Art-Nr. möglich',
     'thumbnail'   => 'out/pictures/ecs.png',
-    'version'     => '2.0.0',
+    'version'     => '0.1',
     'author'      => '<strong style="font-size: 17px;color:#04B431;">Jakob</strong>',
-    'url'         => 'https://',
     'email'       => 'jk@trensco.de',
     'extend'      => array(
 		\OxidEsales\Eshop\Application\Model\Search::class       => \var_art_suche\Model\Search::class,
